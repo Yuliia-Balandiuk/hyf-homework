7cc7dc137000346aae07235a6b535a141b594b53
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-06-03 07:02:58 +0200

    add second function to the candy-halper.js

--- javascript/javascript1/week2/candy-helper.js
+++ javascript/javascript1/week2/candy-helper.js
@@ -20,5 +20,29 @@ addCandy('Chocolate', 50);
 addCandy('Toffee', 10);
 addCandy('Sweet', 40);
 addCandy('ChewingGum', 100);
+addCandy('Toffee', 20);
+addCandy('Sweet', 40);
+addCandy('Chocolate', 50);
+addCandy('Toffee', 10);
 
 console.log(buyCandyPrices);
+
+let amountToSpend = Math.random() * 100;
+let sum = buyCandyPrices[0];
+
+console.log(amountToSpend);
+
+function canBuyMoreCandy() {
+  for (let i = 1; i < buyCandyPrices.length; i++) {
+    if (sum <= amountToSpend) {
+      console.log('You can buy more, so please do!');
+      sum += buyCandyPrices[i];
+      console.log(`You bought ${i} candies`);
+    } else {
+      console.log('Enough candy for you!');
+    }
+  }
+  return sum;
+}
+
+console.log(canBuyMoreCandy());
