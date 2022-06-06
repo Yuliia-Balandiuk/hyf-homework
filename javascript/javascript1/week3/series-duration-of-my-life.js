6a5cab9de92c3bea0585fee26d226ce1b12e637c
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-06-06 21:44:56 +0200

    start series-duration-of-my-life task

---
+++ javascript/javascript1/week3/series-duration-of-my-life.js
@@ -0,0 +1,26 @@
+const seriesDurations = [
+  {
+    title: 'Game of thrones',
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: 'The Walking Dead',
+    days: 7,
+    hours: 23,
+    minutes: 018,
+  },
+  {
+    title: 'Breaking Bad',
+    days: 9,
+    hours: 21,
+    minutes: 48,
+  },
+];
+
+function logOutSeriesText() {
+  // write code here
+}
+
+logOutSeriesText();
