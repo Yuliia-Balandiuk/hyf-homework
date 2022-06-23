29dab68a790fce589f825d47b948bc86a4d69742
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-06-23 11:14:54 +0200

    implement all tasks

---
+++ javascript/javascript2/week1/spirit-animal.js
@@ -0,0 +1,26 @@
+const userNameInput = document.getElementById('nameInput');
+const inputButton = document.getElementById('inputButton');
+const answerName = document.getElementById('answer');
+
+function getSpiritAnimal() {
+  const spiritAnimal = [
+    'The Bengal Cat',
+    'The Black Bear',
+    'The Calico Cat',
+    'The Canada Jay',
+    'The Capuchin Monkey',
+    'The Clouded Leopard',
+    'The Copperhead Snake',
+    'The Crested Caracara',
+    'The Dromedary Camel',
+    'The German Shepherd',
+  ];
+  if (!userNameInput.value) {
+    answerName.innerHTML = 'Please enter your name';
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * spiritAnimal.length);
+  answerName.innerHTML = `${userNameInput.value} - ${spiritAnimal[randomIndex]}`;
+}
+
+inputButton.addEventListener('click', getSpiritAnimal);
