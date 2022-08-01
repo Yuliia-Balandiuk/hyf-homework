3140a5686ae866dca126af5b4e8d75c3d19029fc
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-08-01 21:28:05 +0200

    implement tasks 2

--- javascript/javascript2/week3/warmup.js
+++ javascript/javascript2/week3/warmup.js
@@ -40,18 +40,43 @@ divTask1.appendChild(latitudeParagraph);
 divTask1.appendChild(longitudeParagraph);
 buttonTask5.innerHTML = 'Log location';
 
+let myLatitude;
+let myLongitude;
+
 buttonTask5.addEventListener('click', function () {
   navigator.geolocation.getCurrentPosition(function (position) {
-    let myLatitude = position.coords.latitude;
-    let myLongitude = position.coords.longitude;
+    myLatitude = position.coords.latitude;
+    myLongitude = position.coords.longitude;
     latitudeParagraph.innerHTML = `This is the latitude: ${myLatitude} `;
     longitudeParagraph.innerHTML = `This is the longitude: ${myLongitude}`;
   });
 });
 
-// 6
+// 6 It doesn't work
+
+const mapBtn = document.getElementById('mapBtn');
+
+mapBtn.addEventListener('click', () => {
+  function initMap() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      // center: { lat: myLatitude, lng: myLongitude },
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
+  window.initMap = initMap;
+});
 
 // 7
+function runAfterDelay(delay, callback) {
+  setTimeout(function () {
+    callback();
+  }, delay * 1000);
+}
+
+runAfterDelay(4, function () {
+  console.log('Should be logged after 4 seconds');
+});
 
 // 8
 const myBody = document.querySelector('body');
@@ -59,3 +84,59 @@ const myBody = document.querySelector('body');
 myBody.addEventListener('dblclick', function () {
   console.log('double click!');
 });
+
+// 9
+function logFunnyJoke() {
+  console.log('It is some funny joke))))');
+}
+
+function logBadJoke() {
+  console.log('It is some bad joke((((');
+}
+
+function jokeCreator(shouldTellFunnyJoke, logFunnyJoke, logBadJoke) {
+  if (shouldTellFunnyJoke) {
+    logFunnyJoke();
+  } else {
+    logBadJoke();
+  }
+}
+
+jokeCreator(true, logFunnyJoke, logBadJoke);
+jokeCreator(false, logFunnyJoke, logBadJoke);
+
+// Function as a variable
+// 1
+const arrayOfFunctions = [firstFunction, secondFunction, thirdFunction];
+
+function firstFunction() {
+  console.log('I am first function');
+}
+function secondFunction() {
+  console.log('I am second function');
+}
+function thirdFunction() {
+  console.log('I am third function');
+}
+arrayOfFunctions.forEach((item) => item());
+
+// 2
+const myFunction = () => {
+  console.log('This is function as const');
+};
+myFunction();
+
+function myFunction2() {
+  console.log('This is function as function');
+}
+myFunction2();
+
+// 3
+function printName(userName) {
+  console.log(`My name is ${userName}`);
+}
+const myObj = {
+  text: printName,
+};
+
+myObj[printName('Olga')];
