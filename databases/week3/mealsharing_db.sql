f3db7605624f179e29b1f7c0672fd6afd1bfa875
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-09-03 19:09:02 +0200

    add queries

--- databases/week3/mealsharing_db.sql
+++ databases/week3/mealsharing_db.sql
@@ -32,3 +32,19 @@ CREATE TABLE `review` (
 PRIMARY KEY (`id`),
 CONSTRAINT `fk_meal` FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
+INSERT INTO meal (id, title, description, location, `when`, max_reservations, price, created_date)
+VALUES (1, 'Crawfish soup', 'Traditional soup cooked on crawfish tails. Served with homemade dumplings with crayfish necks', 'Bregnegaardsvej 2A, 2920 Charlottenlund', '2022-08-01 10:00:00', 2, 250.50, '2022-08-01');
+INSERT INTO meal (id, title, description, location, `when`, max_reservations, price, created_date)
+VALUES (2, 'Fish soup', 'Our twist on a Hungarian soup Halaszle. Servered with spicy Eros Pista saus', 'Gl. Hovedvagt, Kastellet 1, 2100 Kobenhavn', '2022-08-05 08:00:00', 4, 100, '2022-08-04');
+INSERT INTO reservation (id, number_of_guests, meal_id, created_date, contact_phonenumber, contact_name, contact_email)
+VALUES (1, 2, 3, '2022-08-11', '+4560443322', 'Chris', 'chris@gmail.com');
+INSERT INTO reservation (id, number_of_guests, meal_id, created_date, contact_phonenumber, contact_name, contact_email)
+VALUES (2, 1, 1, '2022-08-01', '+4560883399', 'Susanne', 'susanne@gmail.com');
+INSERT INTO reservation (id, number_of_guests, meal_id, created_date, contact_phonenumber, contact_name, contact_email)
+VALUES (3, 4, 2, '2022-08-05', '+4560664411', 'Morten', 'morten@gmail.com');
+INSERT INTO reservation (id, number_of_guests, meal_id, created_date, contact_phonenumber, contact_name, contact_email)
+VALUES (4, 4, 3, '2022-08-11', '+4560443300', 'Olga', 'olga@gmail.com');
+INSERT INTO review (id, title, description, meal_id, stars, created_date) 
+VALUES (1, 'Very good', 'Nicely designed place with fancy way of serving dishes. Highly recommended for visitors during weekdays as not so crowded.', 3, 5, '2022-08-14');
+INSERT INTO review (id, title, description, meal_id, stars, created_date) 
+VALUES (2, 'Good', 'Good price for what they offer.', 1, 4, '2022-08-10');
