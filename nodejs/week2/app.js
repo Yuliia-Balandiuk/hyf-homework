74195544d79a8b08946484f9419009cbbc872e4b
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-09-25 13:08:33 +0200

    create search

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const document = require('./documents.json');
+const port = process.env.PORT || 3000;
+const documents = require('./documents.json');
 
-// Support parsing JSON requests
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('This is a search engine');
+});
+
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+
+  if (query) {
+    const result = documents.filter((element) =>
+      Object.values(element).some((item) => String(item).includes(query))
+    );
+    if (result.length === 0) {
+      res.status(404).json({ error: 'Record not found' });
+    }
+    res.send(result);
+  } else {
+    res.send(documents);
+  }
 });
 
 app.listen(port, () => {
