1a4587d947db36a4fb66263d9a76208d503346ee
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-09-28 15:15:50 +0200

    implement POST /search

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -6,7 +6,7 @@ const documents = require('./documents.json');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('This is a search engine');
+  res.send(documents);
 });
 
 app.get('/search', (req, res) => {
@@ -38,7 +38,37 @@ app.get('/documents/:id', (req, res) => {
   }
 });
 
-app.post('/search', (req, res) => {});
+///////////////////////
+app.post('/search', async (req, res) => {
+  try {
+    const body = req.body.fields;
+    const query = req.query.q;
+
+    if (body && query) {
+      res.status(400).json({
+        error: 'Bad Request. You can specify only Query or only Body',
+      });
+    } else if (query) {
+      const result = documents.filter((element) =>
+        Object.values(element).some((item) => String(item).includes(query))
+      );
+      if (result.length === 0) {
+        res.status(404).json({ error: 'Record not found' });
+      }
+      res.send(result);
+    } else if (body) {
+      const result = documents.filter((element) =>
+        Object.keys(body).some((key) => element[key] === body[key])
+      );
+      if (result.length === 0) {
+        res.status(404).json({ error: 'Record not found' });
+      }
+      res.send(result);
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
