8bd5ebafa4d8ed623c95d3d2c83137f5763d9e2c
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-09-28 16:31:04 +0200

    fix after review

--- databases/week2/part-3-more-queries.sql
+++ databases/week2/part-3-more-queries.sql
@@ -2,11 +2,13 @@ SELECT task.title, user.name, user.email FROM `task`
 JOIN user_task ON task.id = user_task.task_id
 JOIN user ON user.id = user_task.user_id
 WHERE user.email REGEXP '@spotify.com';
-SELECT user.name, task.title, status.name
-FROM `user`
-JOIN user_task ON user.id = user_task.user_id
-JOIN task ON task.id = user_task.task_id
-JOIN status ON status.id = task.status_id
+SELECT task.id, task.title AS 'Task Name',
+user.name AS 'user',
+status.name AS 'status'
+FROM user_task
+JOIN task ON user_task.task_id = task.id
+JOIN user ON user_task.user_id = user.id
+JOIN status ON task.status_id = status.id
 WHERE user.name REGEXP 'Donald Duck'
 AND status.name REGEXP 'Not started';
 SELECT user.name, task.title, task.created
