729fa783ad25640dd2352d067a2b425065707638
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-10-21 14:38:18 +0200

    implemented with useState

--- react/react2/week1/github-users-search/src/components/SearchContainer.js
+++ react/react2/week1/github-users-search/src/components/SearchContainer.js
@@ -1,10 +1,38 @@
-import { Text, SearchInput } from './UI';
+import { useState, useEffect } from 'react';
+import { SearchInput } from './UI';
+import UserRow from './UserRow';
 
 const SearchContainer = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUsers = async (query) => {
+    setLoading(true);
+    const API_URL = `https://api.github.com/search/users?q=${query}`;
+    await fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setUsers(data.items))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    setLoading(false);
+  };
+
   return (
     <>
-      <SearchInput type='text' placeholder='Search for user' />
-      <Text></Text>
+      <SearchInput
+        type='text'
+        placeholder='Search for user'
+        onChange={(e) => fetchUsers(e.target.value)}
+      />
+      {loading && <p>LOADING ... </p>}
+      {!users || users.length === 0 ? (
+        <div>No results</div>
+      ) : (
+        users.map((user) => {
+          return <UserRow key={user.id} user={user}></UserRow>;
+        })
+      )}
     </>
   );
 };
--- react/react2/week1/github-users-search/src/components/UI/index.js
+++ react/react2/week1/github-users-search/src/components/UI/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const AppWrapper = styled.div`
-  background-color: #e2c3a4;
+  /* background-color: #e2c3a4; */
   text-align: center;
   font-family: 'Fuzzy Bubbles', cursive;
 `;
@@ -16,7 +16,7 @@ export const SearchInput = styled.input`
   font-family: inherit;
   background: papayawhip;
   border: 0.1rem solid #323045;
-  border-radius: 1rem;
+  border-radius: 3rem 8rem;
   ::placeholder {
     color: #323045;
   }
@@ -26,4 +26,12 @@ export const Text = styled.p`
   font-size: 1.2rem;
   color: #323045;
   font-family: inherit;
+`;
+
+export const RowContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 0.1rem solid #e2c3a4;
+  margin: 1rem 20rem;
+  border-radius: 3rem 10rem; ;
 `;
